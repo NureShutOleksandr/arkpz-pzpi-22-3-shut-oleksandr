86821d3c4130021ce7f7a9edaee26479d1d0b12d
ShutSasha <oleksandr.shut@nure.ua>
2024-11-24 00:38:10 +0200

    feat: added swagger

--- Task2/arkpz-pzpi-22-3-shut-oleksandr-task2/src/main.ts
+++ Task2/arkpz-pzpi-22-3-shut-oleksandr-task2/src/main.ts
@@ -1,8 +1,27 @@
 import { NestFactory } from '@nestjs/core'
-import { AppModule } from './app.module'
+import { SwaggerModule } from '@nestjs/swagger'
+import { config, swaggerPath } from './swagger'
+import { AppModule } from './modules/app/app.module'
+import * as cookieParser from 'cookie-parser'
 
-async function bootstrap() {
+const PORT = process.env.PORT || 5000
+
+async function bootstrap(): Promise<void> {
   const app = await NestFactory.create(AppModule)
-  await app.listen(3000)
+
+  app.setGlobalPrefix('api')
+
+  app.enableCors({
+    credentials: true,
+    origin: true,
+  })
+
+  app.use(cookieParser())
+
+  const document = SwaggerModule.createDocument(app, config)
+  SwaggerModule.setup(swaggerPath, app, document)
+
+  await app.listen(PORT)
 }
+
 bootstrap()
