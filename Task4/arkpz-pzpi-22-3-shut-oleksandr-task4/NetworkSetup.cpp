f35f973f0f446725c24b6801aaa3339f04d973d4
ShutSasha <oleksandr.shut@nure.ua>
2024-12-19 13:00:04 +0200

    feat: added comments to setup function

--- Task4/arkpz-pzpi-22-3-shut-oleksandr-task4/NetworkSetup.cpp
+++ Task4/arkpz-pzpi-22-3-shut-oleksandr-task4/NetworkSetup.cpp
@@ -3,6 +3,8 @@
 const char* WIFI_SSID = "Wokwi-GUEST";
 const char* WIFI_PASSWORD = "";
 
+// Function to initialize and establish a WiFi connection using predefined credentials
+// Continuously attempts to connect until successful and logs the status to the Serial Monitor
 void setupWiFiConnection() {
     WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
 
