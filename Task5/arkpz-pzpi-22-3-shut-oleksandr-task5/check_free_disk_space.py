a528e7e42ef6eb1dca00727bb03920f700452351
ShutSasha <oleksandr.shut@nure.ua>
2024-12-20 20:30:05 +0200

    feat: added free space checker

---
+++ Task5/arkpz-pzpi-22-3-shut-oleksandr-task5/check_free_disk_space.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+import shutil
+
+# Check if there is enough free disk space (at least 400 MB)
+def check_free_disk_space(required_mb=400):
+    disk_usage = shutil.disk_usage(".")
+    free_mb = disk_usage.free // (1024 * 1024)  # Get the free disk space in MB
+    print(f"Available disk space: {free_mb} MB. Required: {required_mb} MB")
+    if free_mb < required_mb:
+        raise Exception(f"Not enough disk space! Required: {required_mb} MB, Available: {free_mb} MB")
+    
+    user_input = input(
+        f"There is enough disk space ({free_mb} MB available). Do you want to proceed with the installation? (yes/no): "
+    ).strip().lower()
+    
+    if user_input not in ['yes', 'y']:
+        raise Exception("Installation cancelled by the user.")
--- Task5/arkpz-pzpi-22-3-shut-oleksandr-task5/setup.py
+++ Task5/arkpz-pzpi-22-3-shut-oleksandr-task5/setup.py
@@ -1,12 +1,19 @@
 #!/usr/bin/env python3
+import sys
 
 from nodejs_installer import nodejs_setup
 from check_database_password import prompt_password
+from check_free_disk_space import check_free_disk_space
 
 # Main entry point for downloading, installing, and verifying Node.js
 def main():
-    prompt_password()
-    nodejs_setup()
+    try:
+        check_free_disk_space()
+        prompt_password()
+        nodejs_setup()
+    except Exception as e:
+        print(f"An error occurred: {e}")
+        sys.exit(1)
 
 if __name__ == "__main__":
     main()
